f3b9798e91ee90fa3f5ebe3dbc1fc46ab3737789
Leo_455 <Leo_455@163.com>
2024-10-21 12:37:22 +0800

    加入网络中断的处理，填写注释，写入文件

--- lynchpin.py
+++ lynchpin.py
@@ -5,19 +5,42 @@ import time
 from plyer import notification #通知弹窗库
 from time import sleep
 
-url = "https://ak.hypergryph.com/lynchpin/api/meta"
-r = requests.get(url)
-responce = r.json()
-number = responce["data"]["progress"]
+url = "https://ak.hypergryph.com/lynchpin/api/meta" #lynchpin URL
+active = True #初始化状态
 
-t = time.strftime("%Y-%m-%d",time.localtime())
+while active == True:
+    try:
+        r = requests.get(url) #请求
+        responce = r.json() #存储
+        active = False #若请求成功则跳出循环
+    except requests.exceptions.ConnectionError as e: #处理网络中断
+        print("网络连接中断，正在重试. . .")
+        sleep(10)
+        continue #重试
 
-print (number)
+number = responce["data"]["progress"] #提取百分数
+t = time.strftime("%Y-%m-%d",time.localtime()) #请求当前日期
+
 #Windows通知弹窗
 notification.notify(
     title = "lynchpin",
     message = f"{t} 的lynchpin是{number}%"
     )
 
-with open("D:\\programing\\python\\pv4\\lynchpin.txt","a",encoding='utf-8') as file:
-    file.write(f"{t} {number}%\n")
+#提取上一次请求的日期
+with open("D:\\programing\\python\\pv4\\latest_date.txt","r",encoding='utf-8') as file:
+    date = file.read()
+
+#判断是否已经存储过今日的响应，若是则不写入
+if date != t:
+    #存储响应结果
+    with open("D:\\programing\\python\\pv4\\lynchpin.txt","a",encoding='utf-8') as file:
+        file.write(f"{t} {number}%\n{responce}\n\n")
+    
+    #覆写今日请求日期
+    with open("D:\\programing\\python\\pv4\\latest_date.txt","w",encoding='utf-8') as file:
+        file.write(f"{t}")
+    
+    print(f"写入完成：{number}%")
+else:
+    print(f"今日写入已记录：{number}%")
