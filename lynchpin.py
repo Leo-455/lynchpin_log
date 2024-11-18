6d11936c7ec4cdfd56848090148c333fd7c1c8b2
Leo_455 <Leo_455@163.com>
2024-11-18 20:41:46 +0800

    更改使用的通知库

--- lynchpin.py
+++ lynchpin.py
@@ -2,31 +2,31 @@
 import requests #api库
 import json
 import time
-from plyer import notification #通知弹窗库
+from win10toast import ToastNotifier #通知弹窗库
 from time import sleep
 
-#url = "https://ak.hypergryph.com/standbyherside/api/premeta" #StandByHerSide API（似乎是忘记密码提示？）
-url = "https://ak.hypergryph.com/lynchpin/api/meta" #lynchpin API
+url_1 = "https://ak.hypergryph.com/standbyherside/api/premeta" #StandByHerSide API（似乎是忘记密码提示？）
+url_2 = "https://ak.hypergryph.com/lynchpin/api/meta" #lynchpin API
 active = True #初始化状态
 
 while active == True:
     try:
-        r = requests.get(url) #请求
-        responce = r.json() #存储
+        r = requests.get(url_1) #请求
+        responce_1 = r.json() #存储
+        r = requests.get(url_2) #请求
+        responce_2 = r.json() #存储
         active = False #若请求成功则跳出循环
     except requests.exceptions.ConnectionError as e: #处理网络中断
         print("网络连接中断，正在重试. . .")
         sleep(10)
         continue #重试
 
-number = responce["data"]["progress"] #提取百分数
+number = responce_2["data"]["progress"] #提取百分数
 t = time.strftime("%Y-%m-%d",time.localtime()) #请求当前日期
 
 #Windows通知弹窗
-notification.notify(
-    title = "lynchpin",
-    message = f"{t} 的lynchpin是{number}%"
-    )
+toaster = ToastNotifier()
+toaster.show_toast("lynchpin",f"{t} 的lynchpin是{number}%\n{responce_1}",duration=10)
 
 #提取上一次请求的日期
 with open("D:\\programing\\python\\pv4\\latest_date.txt","r",encoding='utf-8') as file:
@@ -36,7 +36,7 @@ with open("D:\\programing\\python\\pv4\\latest_date.txt","r",encoding='utf-8') a
 if date != t:
     #存储响应结果
     with open("D:\\programing\\python\\pv4\\lynchpin.txt","a",encoding='utf-8') as file:
-        file.write(f"{t} {number}%\n{responce}\n\n")
+        file.write(f"{t} {number}%\n{responce_2}\n\n")
     
     #覆写今日请求日期
     with open("D:\\programing\\python\\pv4\\latest_date.txt","w",encoding='utf-8') as file:
